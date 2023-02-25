20f58ef351937e4f4b3eced0817fbb16321cdb96
altrono <hbotema16@gmail.com>
2023-02-25 09:38:28 +0200

    fixed bugs plus advanced Auth using headers

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import requests
 import os
-
+os.environ['PIXETOKEN'] = 'awsedddwdsdsfdgfhgjhjhjh'
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+# TOKEN = os.environ.get('PIXETOKEN')
+USERNAME = 'altronos'
+# user_params = {
+#     'token': TOKEN,
+#     'username': USERNAME,
+#     'agreeTermsOfService': 'yes',
+#     'notMinor': 'yes'
+# }
 
-user_params = {
-    'token': os.environ.get('PIXETOKEN'),
-    'username': 'altrono',
-    'agreeTermsOfService': 'yes',
-    'notMinor': 'yes'
+# response = requests.post(PIXELA_ENDPOINT, json=user_params)
+# # print(response.text)
+grapth_endpoint = f'{PIXELA_ENDPOINT}/{USERNAME}/graphs'
+
+grapth_config = {
+    'id': 'graph1',
+    'name': 'Memory focus',
+    'unit':"Km",
+    'type': 'float',
+    'color': 'ajisai'
 }
 
-response = requests.post(PIXELA_ENDPOINT, json=user_params)
-print(response.text)
+headers = {
+    'X-USER-TOKEN': 'awsedddwdsdsfdgfhgjhjhjh'
+}
 
+
+response = requests.post(url=grapth_endpoint, json=grapth_config, headers=headers)
+
+print(response.text)
